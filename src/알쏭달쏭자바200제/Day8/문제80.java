cf7024d03aebd290617cc2c4a9d8cd90be3a92eb
springweb <junseok2217@gmail.com>
2022-06-23 23:37:40 +0900

    문제 71~80

--- src/알쏭달쏭자바200제/Day8/문제80.java
+++ src/알쏭달쏭자바200제/Day8/문제80.java
@@ -15,15 +15,6 @@ public class 문제80 {
     }
 
 
-    public static void main2(String[] args) {
-        System.out.printf("ABC\n");
-        System.out.printf("%d\n" , 12345);
-        System.out.printf("%3d\n", 12345);
-        System.out.printf("%7d\n", 12345);
-        System.out.printf("%5d\n", 123);
-        System.out.printf("%05d\n", 123);
-    }
-
 }
 
 
